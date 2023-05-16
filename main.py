14493e3ef72fb8b079eb265ed265bceea2ad53fe
VictorDolores64 <antonio.arteaga@uabc.edu.mx>
2023-05-15 23:00:15 -0700

    Pruebas Realizadas

---
+++ main.py
@@ -0,0 +1,8 @@
+from art import *
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+print(Fore.RED + 'El profe')
+print(Back.GREEN + 'rifa muchisimo')
+print(Style.RESET_ALL)
+aprint("random")
